d95ca44c4c3f28814550ce6a4e589a4cb3f4af0c
byPronox <bypronoxyt@gmail.com>
2024-10-15 09:01:23 -0500

    Commit 2

---
+++ StefanJativa_TALLER1_Progrmacion4/Models/Equipo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StefanJativa_TALLER1_Progrmacion4.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(100)]
+        public string Ciudad { get; set; }
+        [MaxLength(100)]
+        public string TitulosExtrangeros { get; set; }
+        [MaxLength(100)]
+        public bool AceptaExtrangeros { get; set; }
+    }
+}
---
+++ StefanJativa_TALLER1_Progrmacion4/Models/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StefanJativa_TALLER1_Progrmacion4.Models
+{
+    public class Jugador
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [MaxLength (100)]
+        public string Posicion { get; set; }
+        [Required]
+        [Range(0,110)]
+        public int Edad {  get; set; }
+        [NotNull]
+        [MaxLength()]
+        public string Equipo { get; set; }
+    }
+}
