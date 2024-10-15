8b02137ed40734eb5ff4615a0d8d02987db31631
laboratorio <laboratorio@UPOAULA10306>
2024-10-15 08:42:15 -0500

    Agregar archivos de proyecto.

---
+++ StefanJativa_TALLER1_Progrmacion4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StefanJativa_TALLER1_Progrmacion4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
