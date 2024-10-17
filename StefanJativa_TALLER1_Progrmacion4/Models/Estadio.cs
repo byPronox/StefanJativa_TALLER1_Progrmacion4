bae51f9ab4c8f1b5879a893c134125c84efdd2a3
byPronox <bypronoxyt@gmail.com>
2024-10-17 18:49:15 -0500

    pureba 2

---
+++ StefanJativa_TALLER1_Progrmacion4/Models/Estadio.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StefanJativa_TALLER1_Progrmacion4.Models
+{
+    public class Estadio
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Direccion { get; set; }
+        [AllowNull]
+        [MaxLength (100)]
+        public string Ciudad { get; set; }
+
+        [Required]
+        [Range (0,80000)]
+        public int? Capacidad { get; set; }
+
+        [ForeignKey]
+
+    }
+}
